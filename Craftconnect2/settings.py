a1db404ebb78e772677f4efee9c7b69de4623cef
basit <basitmohammed362@gmail.com>
2024-05-21 13:51:06 +0000

    hello

--- Craftconnect2/settings.py
+++ Craftconnect2/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livereload',
     'myapp'
 ]
 
@@ -48,7 +47,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'livereload.middleware.LiveReloadScript',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
