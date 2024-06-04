eb8fad3663a135483dafa7fa02c5fa4c12eef31c
Emmanuel <sarpbe808@gmail.com>
2024-06-04 13:00:22 +0000

    Add a feature that can hide and show password for ht logins only

--- Craftconnect2/settings.py
+++ Craftconnect2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
     'myapp'
 ]
 
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'livereload.middleware.LiveReloadScript',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
